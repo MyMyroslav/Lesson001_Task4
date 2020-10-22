10487f1a1aa80078f169ffdb82d8934a52d29132
Myroslav <havrushkomyroslav@gmail.com>
2020-10-22 21:01:48 +0300

    Done-Lesson001_Task4

---
+++ Lesson001_Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lesson001_Task4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double dblRes = 1.0 / 2.0;
+            float fRes = 1.0f / 2.0f;
+            Console.WriteLine(" Double 1/2 = " + dblRes);
+            Console.WriteLine(" Float 1/2 = " + fRes);
+        }
+    }
+}
